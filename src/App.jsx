7795ebdde3feaba2464a4573b9b6b76f5c4f212b
yasharma <yashsharma205@gmail.com>
2018-04-22 18:48:52 +0530

    Working theme sidebar minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router} from './router';
+import {Router} from './router.jsx';
 
 const App = props => {
     const { history } = props;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
+import App from './App.jsx';
 import {store, history} from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
+
+// Custom stylesheet
+import './assets/css/style.css';
 
 ReactDOM.render(
   	<Provider store={store}>
